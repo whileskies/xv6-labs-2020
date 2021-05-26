5bcd5bce356cb7562912502d8bc77a812006a70a
whileskies <gt110@qq.com>
2021-05-26 19:45:16 +0800

    finish Uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,52 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
-              
+extern void thread_switch(struct context*, struct context*);
+
+void
+debug_thread_state()
+{
+  printf("thread state: \n");
+  for(int i = 0; i < MAX_THREAD; i++){
+    struct thread *t = all_thread + i;
+    if(t->state == FREE) {
+      printf("%d: FREE\n", i);
+    } else if(t->state == RUNNABLE) {
+      printf("%d: RUNNABLE\n", i);
+    } else if(t->state == RUNNING) {
+      printf("%d: RUNNING\n", i);
+    }
+  }
+}
+
+
 void 
 thread_init(void)
 {
@@ -36,6 +72,9 @@ void
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
+
+  //printf("schedule\n");
+  //debug_thread_state();
 
   /* Find another runnable thread. */
   next_thread = 0;
@@ -63,6 +102,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &next_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +117,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.sp = (uint64)t->stack + STACK_SIZE;
+  t->context.ra = (uint64)func;
 }
 
 void 
