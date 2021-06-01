9649b25e559ad9f7991fdcf41a0b694c7ee649eb
whileskies <gt110@qq.com>
2021-06-01 09:27:02 +0800

    finish Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,19 +14,25 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
+#define LOCK_NAME_N 6
+
 struct run {
   struct run *next;
 };
 
 struct {
   struct spinlock lock;
+  char lock_name[LOCK_NAME_N];
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++) {
+    snprintf(kmem[i].lock_name, LOCK_NAME_N, "kmem%d", i);
+    initlock(&kmem[i].lock, kmem[i].lock_name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +62,13 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,13 +78,32 @@ void *
 kalloc(void)
 {
   struct run *r;
+  
+  push_off();
+  int cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+  r = kmem[cpu].freelist;
+  if(r) {
+    kmem[cpu].freelist = r->next;
+    release(&kmem[cpu].lock);
+  }else {
+    release(&kmem[cpu].lock);
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
+    for(int nextid = 0; nextid < NCPU; nextid++) {
+      if(cpu != nextid) {
+        acquire(&kmem[nextid].lock);
+        r = kmem[nextid].freelist;
+        if(r) {
+          kmem[nextid].freelist = r->next;
+          release(&kmem[nextid].lock);
+          break;
+        }
+        release(&kmem[nextid].lock);
+      }
+    }
+  }
+  pop_off();
+  
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
